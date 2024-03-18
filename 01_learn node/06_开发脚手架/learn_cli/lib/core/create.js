5434216740b2e2e85d8597a733832e1c04f79484
黄张欢 <huangzh@fline88.com>
2024-03-18 15:07:32 +0800

    feat: 脚手架

---
+++ 01_learn node/06_开发脚手架/learn_cli/lib/core/create.js
@@ -0,0 +1,13 @@
+const program = require('commander')
+const {
+    createProjectAction
+} = require('./action')
+
+const createCommand = () =>{
+    program
+        .command('create <project> [others...]')
+        .description('clone repository into a folder')
+        .action(createProjectAction)
+}
+
+module.exports = createCommand
