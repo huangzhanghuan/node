ee1de090e39d233707cbddeebf28bd2ead98e3eb
黄张欢 <huangzh@fline88.com>
2024-03-20 14:39:46 +0800

    feat: 脚手架

--- 01_learn node/06_开发脚手架/learn_cli/lib/core/action.js
+++ 01_learn node/06_开发脚手架/learn_cli/lib/core/action.js
@@ -1,9 +1,11 @@
 const { promisify } = require('util')
+const path = require('path');
 
 const download = promisify(require('download-git-repo'))
 
 const { vueRepo } = require('../config/repo.config.js')
-const { commandSpawn } = require('../utils/terminal.ts')
+const { commandSpawn } = require('../utils/terminal.js')
+const { compile, writeToFile, createDirSync } = require('../utils/utils.js')
 
 // callback -> promisify(函数) -> Promise -> async await
 const createProjectAction = async (project) => {
@@ -20,13 +22,53 @@ const createProjectAction = async (project) => {
     await commandSpawn(command, ['run', 'serve'], { cwd: `./${project}` });
 }
 
-const addCpnAction = (name, dest) => {
-    
-    
+// 添加组件的action
+const addComponentAction = async(name, dest) => {
+    // 1.有对应的ejs模块
+    // 2.编译ejs模块 result
+    const result = await compile("vue-component.ejs", { name, lowerName: name.toLowerCase() });
+
+    // 3.写入文件的操作
+    const targetPath = path.resolve(dest, `${name}.vue`);
+    writeToFile(targetPath, result);  
+}
+
+// 添加组件和路由
+const addPageAndRouteAction = async (name, dest) => {
+    // 1.编译ejs模版
+    const data = { name, lowerName: name.toLowerCase() }
+    const pageResult = await compile('vue-component.ejs', data);
+    const routeResult = await compile('vue-router.ejs', data);
+
+    // 3.写入文件
+    if (createDirSync(dest)) {
+        const targetPagePath = path.resolve(dest, `${name}.vue`);
+        const targetRoutePath = path.resolve(dest, `router.js`);
+
+        writeToFile(targetPagePath, pageResult);
+        writeToFile(targetRoutePath, routeResult);
+    }
+}
+
+const addStoreAction = async (name, dest) => {
+    // 1.编译的过程
+    const storeResult = await compile('vue-store.ejs', {});
+    const typesResult = await compile('vue-types.ejs', {});
+
+    // 2.创建文件
+    if (createDirSync(dest)) {
+        const targetStorePath = path.resolve(dest, `${name}.js`);
+        const targetTypePath = path.resolve(dest, `types.js`);
+
+        writeToFile(targetStorePath, storeResult);
+        writeToFile(targetTypePath, typesResult);
+    }
 }
 
 
-
 module.exports = {
-    createProjectAction
+    createProjectAction,
+    addComponentAction,
+    addPageAndRouteAction,
+    addStoreAction
 }
--- 01_learn node/06_开发脚手架/learn_cli/lib/core/create.js
+++ 01_learn node/06_开发脚手架/learn_cli/lib/core/create.js
@@ -1,7 +1,9 @@
 const program = require('commander')
 const {
     createProjectAction,
-    addCpnAction
+    addComponentAction,
+    addPageAndRouteAction,
+    addStoreAction
 } = require('./action')
 
 const createCommand = () =>{
@@ -9,11 +11,25 @@ const createCommand = () =>{
         .command('create <project> [others...]')
         .description('clone repository into a folder')
         .action(createProjectAction)
-    
-     program
+    program
         .command('addcpn <name>')
-        .description('add vue component, 例如 learn_cli addcpn Helloworld -d src/components')
-        .action(addCpnAction)
+        .description('add vue component, 例如 learn_cli addcpn Helloworld [-d src/components]')
+        .action((name) => {
+            addComponentAction(name, program.dest || 'src/components')
+        });
+    program
+        .command('addpage <page>')
+        .description('add vue page and router config, 例如: why addpage Home [-d src/pages]')
+        .action((page) => {
+            addPageAndRouteAction(page, program.dest || `src/pages/${page}`);
+        })
+    program
+        .command('addstore <store>')
+        .description('add vue page and router config, 例如: why addstore Home [-d src/pages]')
+        .action((store) => {
+            addStoreAction(store, program.dest || `src/store/modules/${store}`);
+
+        })
 }
 
 module.exports = createCommand
